2af08fc9b6ee0d2b8c0ee9504420dcc655754570
suman-stha <sthasum5@gmail.com>
2023-10-27 08:19:54 -0400

    qrcode

---
+++ generator.py
@@ -0,0 +1,17 @@
+import qrcode
+#  pip install qrcode
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L, #other contstants (M, Q, H)
+    box_size=10,
+    border=4,
+)
+
+qr.add_data('https://suman-stha.github.io/menu/')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color='black', back_color='white')
+img.save('qrtest.png')
+
+
+
